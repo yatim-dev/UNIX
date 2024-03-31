3d2cfbb3253e7bb64c0b656f0d9f2868618db926
yatim <yatim@yatims-comp.yatims-link>
2024-03-31 13:01:04 +0500

    del file

--- Makefile
+++
@@ -1,5 +0,0 @@
-all:
-	gcc -o run/task1 task1.c
-
-clean:
-	rm -rf run/*
--- runme.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-if [ ! -d run ]; then
-    mkdir run
-fi
-if [ ! -f run/A ]; then
-    python3 create_file.py
-fi
-make
