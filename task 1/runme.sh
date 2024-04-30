6969eb86ae990d270927b2b259f87bb2864b7934
yatim <yatim@yatims-comp.yatims-link>
2024-04-30 18:33:25 +0500

    change work with dir

--- task 1/runme.sh
+++ task 1/runme.sh
@@ -8,11 +8,11 @@ mkdir run
 python3 create_file.py
 make
 
-./run/task1 run/A run/B
+./task1 run/A run/B
 gzip -k run/A
 gzip -k run/B
-gzip -cd run/B.gz | ./run/task1 run/C
-./run/task1 run/A run/D -b 100
+gzip -cd run/B.gz | ./task1 run/C
+./task1 run/A run/D -b 100
 
 echo -e $(stat -c "Filename: %n \t Size: %s. Blocks: %b. Bytes in block: %B" run/A)
 echo -e $(stat -c "Filename: %n \t Size: %s. Blocks: %b. Bytes in block: %B" run/A.gz)
